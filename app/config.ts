26f162d21376ae408147cc825278faa56ab382f1
Msouss <marilenasoussani@gmail.com>
2024-12-05 14:29:42 -0500

    Update Title

--- app/config.ts
+++ app/config.ts
@@ -9,8 +9,8 @@ export const metaData = {
 
 export const socialLinks = {
   twitter: "https://x.com/1tssirius",
-  github: "https://github.com/1msirius/Nextfolio",
+  github: "https://github.com/Msouss",
   instagram: "https://www.instagram.com/",
-  linkedin: "https://www.linkedin.com/",
-  email: "mailto:example@gmail.com",
+  linkedin: "https://www.linkedin.com/in/marilenasoussani/",
+  email: "marilenasoussani@gmail.com",
 };
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
     <section>
       <a href={socialLinks.twitter} target="_blank">
         <Image
-          src="/profile.png"
+          src="/MarilenaProfile.png"
           alt="Profile photo"
           className="rounded-full bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5 grayscale hover:grayscale-0"
           unoptimized
@@ -17,7 +17,7 @@ export default function Page() {
       </a>
 
       <h1 className="mb-8 text-2xl font-medium tracking-tight">
-        Portfolio, made simple!
+        Marilena Soussani
       </h1>
 
       <div className="prose prose-neutral dark:prose-invert">
