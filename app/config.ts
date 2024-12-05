373b7f121b570c7a46ea75217ddd1da0152971e8
Msouss <marilenasoussani@gmail.com>
2024-12-05 14:47:09 -0500

    update footer

--- app/config.ts
+++ app/config.ts
@@ -1,6 +1,6 @@
 export const metaData = {
   baseUrl: "https://nextfolio-template.vercel.app/",
-  title: "Nextfolio",
+  title: "Marilena Soussani",
   name: "Sirius",
   ogImage: "/opengraph-image.png",
   description:
--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,38 @@ export default function Page() {
         <p>
           Adept at building efficient, user-friendly applications, 
           Marilena is driven by the challenge of creating innovative software solutions.
-          
+          {/*<a
+            target="_blank"
+            href="https://github.com/1msirius/Nextfolio?tab=readme-ov-file#features"
+          >
+            more
+          </a>
+          .
+        </p>
+        <p>
+          Nextfolio is{" "}
+          <a href={socialLinks.github} target="_blank">
+            open-source
+          </a>{" "}
+          and fully customizable, making it easy to add more features.
+        </p>
+        <p>
+          <a
+            href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2F1msirius%2FNextfolio"
+            target="_blank"
+          >
+            Deploy
+          </a>{" "}
+          your Nextfolio site with Vercel in minutes and follow the set up
+          instructions in the{" "}
+          <a href="/blog/getting-started">Getting Started</a> post.
+        </p>
+        <p>
+          Built and maintained by{" "}
+          <a href="https://imsirius.xyz/" target="_blank">
+            Sirius
+          </a>
+          .*/}
         </p>
       </div>
     </section>
