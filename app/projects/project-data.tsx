fcf72205699d6c54d6ab479b1621211cbd78cc69
Msouss <marilenasoussani@gmail.com>
2024-12-05 16:02:34 -0500

    feat: update nav bar and projects

--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -7,24 +7,24 @@ export interface Project {
 
 export const projects: Project[] = [
   {
-    title: "Project One",
+    title: "Calculator iOS",
     year: 2023,
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam veritatis dolorem rem praesentium dicta labore, at laudantium quisquam.",
-    url: "https://example.com/",
+      "This SwiftUI Calculator project is designed to create a functional and visually appealing calculator app for iOS devices. It incorporates basic arithmetic operations and a user-friendly interface while showcasing the use of SwiftUI's declarative syntax and state management capabilities.",
+    url: "https://github.com/Msouss/Ios",
   },
   {
-    title: "Project Two",
+    title: "Cinema app",
     year: 2022,
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam veritatis dolorem rem praesentium dicta labore, at laudantium quisquam.",
-    url: "https://example.com/",
+      "The Cinema app is an Android application built in Java that allows users to browse, search, and explore information about movies. The app aims to replicate the core functionalities of platforms like IMDb, offering a clean and user-friendly interface. It serves as a resource for movie enthusiasts to discover new films, check ratings, view trailers, and read reviews.",
+    url: "https://github.com/Msouss/Java",
   },
   {
-    title: "Project Three",
+    title: "PHP game",
     year: 2021,
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam veritatis dolorem rem praesentium dicta labore, at laudantium quisquam.",
+      "This project is a dynamic web application built in PHP that challenges users with a series of puzzles while incorporating a \"life system\" to increase the game's challenge and engagement. The platform combines HTML, CSS, and JavaScript for the front-end with PHP and a database (like MySQL) for back-end logic and user data management.",
     url: "https://example.com/",
   },
 ];
