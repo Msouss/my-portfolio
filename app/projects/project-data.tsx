d0de4dbab33b1f78bcbcbfeaa57c3bece55b5fdf
Msouss <marilenasoussani@gmail.com>
2025-02-24 21:16:11 -0500

    added more projects

--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -17,14 +17,49 @@ export const projects: Project[] = [
     title: "Cinema App",
     year: 2024,
     description:
-      "The cinema is an app using JavaFX that allows the user to selects movies and seats to reserve their order.",
-    url: "https://github.com/Msouss/Java",
+      "Developed a Movie Reservation System using JavaFX, allowing users to browse movies, select showtimes, and reserve seats seamlessly. Designed an intuitive and interactive UI for a smooth booking experience. Implemented seat selection, order confirmation, and real-time availability updates for an efficient reservation process.",
+    url: "https://github.com/Msouss/Java-Cinema",
   },
   {
-    title: "PHP game",
+    title: "PHP Game",
     year: 2024,
     description:
       "This project is a dynamic web application built in PHP that challenges users with a series of puzzles while incorporating a \"life system\" to increase the game's challenge and engagement. The platform combines HTML, CSS, and JavaScript for the front-end with PHP and a database (like MySQL) for back-end logic and user data management.",
-    url: "https://github.com/Msouss/Php",
+    url: "https://github.com/Msouss/Php-Game",
+  },
+  {
+    title: "Facebook Clone",
+    year: 2024,
+    description:
+      "Developed a fully functional Facebook clone using Swift for iOS, featuring user authentication, profile creation, posts, likes, comments, and real-time updates. Designed a responsive UI with a smooth user experience, implemented an intuitive navigation system, and optimized performance for seamless social interactions.",
+    url: "https://github.com/Msouss/IOS-FacebookClone",
+  },
+  {
+    title: "Multitier App",
+    year: 2024,
+    description:
+      "This project involves developing a multi-tier application using SQL Server, C# Windows Forms, and ADO.NET. The application will manage data for a college system, consisting of programs, courses, students, and enrollments.",
+    url: "https://github.com/Msouss/Multitier-C",
+  },
+  {
+    title: "Ecommerce App",
+    year: 2024,
+    description:
+      "Developed a fully functional e-commerce app in Swift, allowing users to browse products, add items to their cart, and proceed to checkout seamlessly. Designed a clean and modern UI for an intuitive shopping experience. Implemented smooth navigation and efficient cart management for a user-friendly experience.",
+    url: "https://github.com/Msouss/IOS-EcommerceApp",
+  },
+  {
+    title: "Shopping App",
+    year: 2024,
+    description:
+      "Built a user-friendly shopping app in Swift that allows users to browse, select, and purchase fruits and vegetables. Implemented an intuitive cart system for adding and removing items, along with a smooth checkout process. Designed a clean and responsive UI for an engaging shopping experience.",
+    url: "https://github.com/Msouss/IOS-ShoppingApp",
+  },
+  {
+    title: "Hotel App",
+    year: 2024,
+    description:
+      "Developed a Hotel Management System using Python, allowing users to make reservations, check room availability, and manage bookings efficiently. Implemented a user-friendly interface with smooth navigation for both customers and hotel staff. Integrated data handling for storing guest information, reservations, and room details.",
+    url: "https://github.com/Msouss/Python-HotelApp",
   },
 ];
